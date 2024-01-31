a3a1dc80662bc116fcb1593e4a1e3ace3e4b1402
SamG1234 <np03cs4s210297@heraldcollege.edu.np>
2024-01-31 21:57:13 +0545

    app

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 
 # Install requirements
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Collect static files
-python3 manage.py collectstatic
+python manage.py collectstatic
